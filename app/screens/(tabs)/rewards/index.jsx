504b6913f60fb6777ae19c8a8665937794e9e1c5
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-04-13 00:20:18 -0700

    added the activity section to rewards

--- app/screens/(tabs)/rewards/index.jsx
+++ app/screens/(tabs)/rewards/index.jsx
@@ -5,7 +5,9 @@ import { TextButton } from "@components/button/text-btn";
 import { Text } from "@components/text";
 import { colors } from "@constants/colors";
 import { useRefetchOnFocus } from "@hooks/common/use-refetch-on-focus";
+import { FlashList } from "@shopify/flash-list";
 import { nFormatter } from "@utils/common";
+import dayjs from "dayjs";
 import { useMemo } from "react";
 import { ScrollView, View } from "react-native";
 import { useQuery } from "react-query";
@@ -54,7 +56,7 @@ export default function RewardsScreen() {
             >
               <Text>
                 <Text size="3xl">{totalPoints.value}</Text>
-                <Text size="xl">{totalPoints.symbol}</Text>
+                <Text size="lg">{totalPoints.symbol}</Text>
               </Text>
               <Text size="md">Points</Text>
             </View>
@@ -124,36 +126,51 @@ export default function RewardsScreen() {
                   <Text style={{ textAlign: "right" }}>Redeem</Text>
                 </TextButton>
               </View>
-              <View
-                style={{
-                  marginTop: 20,
-                  height: 120,
-                  backgroundColor: colors.cardBg,
-                  padding: 20,
-                  borderRadius: 8,
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "flex-end",
-                }}
-              >
-                <View
-                  style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between",
+              <View style={{ marginTop: 20, height: "100%" }}>
+                <Text size="2xl">Activity</Text>
+                <FlashList
+                  estimatedItemSize={rewards?.length ?? 10}
+                  data={rewards}
+                  keyExtractor={(item) => item.rewardId}
+                  renderItem={({ item }) => {
+                    return (
+                      <View
+                        style={{
+                          padding: 20,
+                          height: 140,
+                          backgroundColor: colors.cardBg,
+                          marginVertical: 10,
+                          borderRadius: 8,
+                        }}
+                      >
+                        <Text color={colors.yellow} bold>
+                          On{" "}
+                          {dayjs(item.createdAt).format(
+                            "MMM DD, YYYY - h:mm a"
+                          )}
+                        </Text>
+                        <Text bold style={{ marginVertical: 5 }}>
+                          Amount paid: ${item.amount / 100}
+                        </Text>
+                        <Text bold style={{ marginVertical: 5 }}>
+                          Points earned: {item.points}
+                        </Text>
+                        <View
+                          style={{
+                            marginVertical: 5,
+                            flexDirection: "row",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          <Text color={colors.inputPlaceholderColor}>
+                            For: {item.rewardFor}
+                          </Text>
+                        </View>
+                      </View>
+                    );
                   }}
-                >
-                  <Text bold size="md">
-                    LYFT
-                  </Text>
-                  <Text bold size="md">
-                    10% off on 3 rides
-                  </Text>
-                </View>
-
-                <TextButton style={{ marginTop: 25, alignSelf: "flex-end" }}>
-                  <Text style={{ textAlign: "right" }}>Redeem</Text>
-                </TextButton>
+                />
               </View>
             </>
           ) : (
